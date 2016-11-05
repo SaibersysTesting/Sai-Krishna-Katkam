d4622bf82067a04576acf864e0b872148160b34c
arun teja <training.saibersys9@gmail.com>
2016-11-05 00:28:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Constructor of Base Class");
+        }
+        public void DisplayMessage()
+        {
+            Console.WriteLine("Hello, how are you?");
+        }
+    }
+
+    public class Child : Base
+    {
+        public Child()
+        {
+            Console.WriteLine("Constructor of Child class");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Child objChild = new Child();
+            //Child class don't have DisplayMessage() method but we inherited from "Base" class
+            objChild.DisplayMessage();
+            Console.Read();
+        }
+    }
+}
