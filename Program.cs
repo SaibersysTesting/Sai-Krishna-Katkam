04f227696dfa73d9d0d39cba64eb36a7584d8c05
arun teja <training.saibersys9@gmail.com>
2016-11-05 11:30:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Base
+    {
+        public virtual string CreatorName()
+        {
+            return "Sai";
+        }
+    }
+    public class Child : Base
+    {
+        //same method name with same signature/parameters
+        public override string CreatorName()
+        {
+            return "SaiKrishna Katkam";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Base objBase = new Child();
+            Console.WriteLine(objBase.CreatorName());
+            Console.Read();
+        }
+    }
+}
